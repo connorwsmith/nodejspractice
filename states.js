78e810a37d7e866b63e3a0419e48a35a1212fb5a
connorwsmith <connor@ciphertek.com>
2024-03-07 15:11:47 -0600

    Wrote the same output in OOP
    I re-factored the code to where it is OOP.

--- states.js
+++ states.js
@@ -9,19 +9,49 @@
 
 const fs = require('fs');
 
-fs.readFile('states.csv', 'utf-8', (err, data) => {
-    if(err) {
-        console.log(err);
-        return;
+class reader {
+    constructor(fileName) {
+        this.fileName = fileName;
     }
 
-    const arrayLine = data.split('\n');
+    splitter(callback) {
+        fs.readFile(this.fileName, 'utf-8', (err, data) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
 
-    arrayLine.forEach((line) => {
-        const part = line.split(',');
+            const arrayLine = data.split('\n');
+            const result = [];
+            for(let line of arrayLine) {
+                const parts = line.split(',');
+                result.push(parts[0]);
+            }
+            callback(result);
+        });
+    }
+}
 
-        if(part.length > 1) {
-            console.log(part[0]);
-        }
-    });
-});
+const read = new reader('states.csv');
+read.splitter((result) => {
+    console.log(result);
+})
+
+// const fs = require('fs');
+
+// fs.readFile('states.csv', 'utf-8', (err, data) => {
+//     if(err) {
+//         console.log(err);
+//         return;
+//     }
+
+//     const arrayLine = data.split('\n');
+
+//     arrayLine.forEach((line) => {
+//         const part = line.split(',');
+
+//         if(part.length > 1) {
+//             console.log(part[0]);
+//         }
+//     });
+// });
